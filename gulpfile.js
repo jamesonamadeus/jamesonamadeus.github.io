a56131b5fc1d48c321af1541e19b31eb39423880
jamesonamadeus <jamesonamadeus@gmail.com>
2017-11-10 05:42:43 -0500

    add in the gulp sass watch. update index

--- gulpfile.js
+++ gulpfile.js
@@ -1,8 +1,15 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 
+//gulp sass
 gulp.task('sass', function(){
     return gulp.src('app/scss/main.scss')
       .pipe(sass())
       .pipe(gulp.dest('app/css'))
 });
+
+//gulp watch
+gulp.task('watch', function(){
+    gulp.watch('app/scss/**/*.scss', ['sass']); 
+
+})
