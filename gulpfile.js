4ff348fd917b013d5b9218cddba4f1328eee4245
jamesonamadeus <jamesonamadeus@gmail.com>
2017-11-11 23:59:26 -0500

    add in the html & js files for reload

--- gulpfile.js
+++ gulpfile.js
@@ -16,4 +16,6 @@ gulp.task('sass', function() {
 gulp.task('watch', ['browserSync', 'sass'], function (){
     gulp.watch('app/scss/**/*.scss', ['sass']); 
 
+    gulp.watch('app/*.html', browserSync.reload); 
+    gulp.watch('app/js/**/*.js', browserSync.reload); 
 });
