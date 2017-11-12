d6c4800b9a5984e70d0e291913ea5904b955c8e2
jamesonamadeus <jamesonamadeus@gmail.com>
2017-11-11 23:54:37 -0500

    add in browsersync

--- gulpfile.js
+++ gulpfile.js
@@ -1,15 +1,19 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
+var browserSync = require('browser-sync').create();
 
 //gulp sass
-gulp.task('sass', function(){
-    return gulp.src('app/scss/main.scss')
+gulp.task('sass', function() {
+    return gulp.src('app/scss/**/*.scss') 
       .pipe(sass())
       .pipe(gulp.dest('app/css'))
+      .pipe(browserSync.reload({
+        stream: true
+      }))
 });
 
 //gulp watch
-gulp.task('watch', function(){
+gulp.task('watch', ['browserSync', 'sass'], function (){
     gulp.watch('app/scss/**/*.scss', ['sass']); 
 
-})
+});
