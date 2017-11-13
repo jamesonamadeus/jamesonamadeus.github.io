d8f8d305f9ee9394d06ccd2f9aee75cbdd5f9cec
jamesonamadeus <jamesonamadeus@gmail.com>
2017-11-13 00:02:10 -0500

    add in js concatenate

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,7 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var browserSync = require('browser-sync').create();
+var useref = require('gulp-useref');
 
 //gulp sass
 gulp.task('sass', function() {
@@ -18,4 +19,11 @@ gulp.task('watch', ['browserSync', 'sass'], function (){
 
     gulp.watch('app/*.html', browserSync.reload); 
     gulp.watch('app/js/**/*.js', browserSync.reload); 
+});
+
+//gulp concatenate
+gulp.task('useref', function(){
+    return gulp.src('app/*.html')
+      .pipe(useref())
+      .pipe(gulp.dest('dist'))
 });
