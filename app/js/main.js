8c5cfed3424c930aa41d058db81499983aaf62f4
jamesonamadeus <jamesonamadeus@gmail.com>
2017-11-27 23:07:52 -0500

    restructure the naming of nav-menu

--- app/js/main.js
+++ app/js/main.js
@@ -1,8 +1,8 @@
 (function() {
     var body = document.body;
-    var burgerMenu = document.getElementsByClassName('brgr-menu')[0];
-    var burgerContain = document.getElementsByClassName('brgr-container')[0];
-    var burgerNav = document.getElementsByClassName('brgr-nav')[0];
+    var burgerMenu = document.getElementsByClassName('nav-menu')[0];
+    var burgerContain = document.getElementsByClassName('nav-contain')[0];
+    var burgerNav = document.getElementsByClassName('nav-list')[0];
   
     burgerMenu.addEventListener('click', function toggleClasses() {
       [body, burgerContain, burgerNav].forEach(function (el) {
