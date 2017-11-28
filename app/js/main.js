50f75f5449b743e2d4697abcc43086c57b654a08
jamesonamadeus <jamesonamadeus@gmail.com>
2017-11-27 21:20:02 -0500

    brgr script

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,12 @@
-var body = document.body;
-var burgerMenu = document.getElementsByClassName('brgr-menu')[0];
-var burgerContain = document.getElementsByClassName('brgr-container')[0];
-var burgerNav = document.getElementsByClassName('brgr-nav')[0];
+(function() {
+    var body = document.body;
+    var burgerMenu = document.getElementsByClassName('brgr-menu')[0];
+    var burgerContain = document.getElementsByClassName('brgr-container')[0];
+    var burgerNav = document.getElementsByClassName('brgr-nav')[0];
+  
+    burgerMenu.addEventListener('click', function toggleClasses() {
+      [body, burgerContain, burgerNav].forEach(function (el) {
+        el.classList.toggle('open');
+      });
+    }, false);
+})();
