f1d4adbfd9569a08af5b28c462f78f1ec2e2783b
jamesonamadeus <jamesonamadeus@gmail.com>
2017-11-27 21:17:48 -0500

    add in brgr menu variables

---
+++ app/js/main.js
@@ -0,0 +1,4 @@
+var body = document.body;
+var burgerMenu = document.getElementsByClassName('brgr-menu')[0];
+var burgerContain = document.getElementsByClassName('brgr-container')[0];
+var burgerNav = document.getElementsByClassName('brgr-nav')[0];
