f56b07be2c149cf6f6a6650afd474bfd522a8c82
jamesonamadeus <jamesonamadeus@gmail.com>
2017-12-26 03:30:59 -0500

    fix up the scss files

--- app/js/main.js
+++ app/js/main.js
@@ -1,20 +1,24 @@
 //burger
 (function() {
-    var body = document.body;
-    var burgerMenu = document.getElementsByClassName('nav-menu')[0];
-    var burgerContain = document.getElementsByClassName('nav-contain')[0];
-    var burgerNav = document.getElementsByClassName('nav-list')[0];
-  
-    burgerMenu.addEventListener('click', function toggleClasses() {
-      [body, burgerContain, burgerNav].forEach(function (el) {
-        el.classList.toggle('open');
-      });
-    }, false);
+	var body = document.body;
+	var burgerMenu = document.getElementsByClassName('nav-menu')[0];
+	var burgerContain = document.getElementsByClassName('nav-contain')[0];
+	var burgerNav = document.getElementsByClassName('nav-list')[0];
+
+	burgerMenu.addEventListener(
+		'click',
+		function toggleClasses() {
+			[body, burgerContain, burgerNav].forEach(function(el) {
+				el.classList.toggle('open');
+			});
+		},
+		false
+	);
 })();
 
 //smooth scrolling
-var scroll = new SmoothScroll('a[href*="#"]',{
-    speed: 1000,
-    offset: -30,
-    easing: 'easeInOutQuad'
+var scroll = new SmoothScroll('a[href*="#"]', {
+	speed: 1000,
+	offset: -30,
+	easing: 'easeInOutQuad'
 });
