500d7084d606fadbc4095e7af47cb88ba3efd2cf
jamesonamadeus <jamesonamadeus@gmail.com>
2017-12-29 19:30:43 -0500

    add in javascript hide and show for elements for home

--- app/js/main.js
+++ app/js/main.js
@@ -24,40 +24,50 @@ var scroll = new SmoothScroll('a[href*="#"]', {
 });
 
 //
-var tmax_optionsGlobal = {
-	repeat: -1,
-	repeatDelay: 0.65,
-	yoyo: true
-};
+el = document.getElementById('hereNow');
+face = document.getElementById('face');
+el.addEventListener(
+	'click',
+	function() {
+		face.style.display = 'block';
 
-CSSPlugin.useSVGTransformAttr = true;
+		var tmax_optionsGlobal = {
+			repeat: -1,
+			repeatDelay: 0.65,
+			yoyo: true
+		};
 
-var tl = new TimelineMax(tmax_optionsGlobal),
-	path = '#face *',
-	stagger_val = 0.009,
-	duration = 5;
+		CSSPlugin.useSVGTransformAttr = true;
 
-$.each($(path), function(i, el) {
-	tl.set($(this), {
-		x: '+=' + getRandom(-1000, 1000),
-		y: '+=' + getRandom(-1000, 1000),
-		rotation: '+=' + getRandom(-720, 720),
-		scale: 0,
-		opacity: 0
-	});
-});
+		var tl = new TimelineMax(tmax_optionsGlobal),
+			path = '#face *',
+			stagger_val = 0.009,
+			duration = 5;
 
-var stagger_opts_to = {
-	x: 0,
-	y: 0,
-	opacity: 1,
-	scale: 1,
-	rotation: 0,
-	ease: Power4.easeOut
-};
+		$.each($(path), function(i, el) {
+			tl.set($(this), {
+				x: '+=' + getRandom(-1000, 1000),
+				y: '+=' + getRandom(-1000, 1000),
+				rotation: '+=' + getRandom(-720, 720),
+				scale: 0,
+				opacity: 0
+			});
+		});
 
-tl.staggerTo(path, duration, stagger_opts_to, stagger_val);
+		var stagger_opts_to = {
+			x: 0,
+			y: 0,
+			opacity: 1,
+			scale: 1,
+			rotation: 0,
+			ease: Power4.easeOut
+		};
 
-function getRandom(min, max) {
-	return Math.random() * (max - min) + min;
-}
+		tl.staggerTo(path, duration, stagger_opts_to, stagger_val);
+
+		function getRandom(min, max) {
+			return Math.random() * (max - min) + min;
+		}
+	},
+	false
+);
